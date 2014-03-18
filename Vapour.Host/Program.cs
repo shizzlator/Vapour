ccc183d8ebc1cd07ebeb14087c50e81d67772a81
David Miranda <davidmiranda11@gmail.com>
2014-03-18 22:39:14 +0000

    added Hello World OWIN app

---
+++ Vapour.Host/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Owin.Hosting;
+using Owin;
+
+namespace Vapour.Host
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string uri = "http://localhost:8080";
+
+            using (WebApp.Start<Startup>(uri))
+            {
+                Console.WriteLine("Started!");
+                Console.ReadKey();
+                Console.WriteLine("Stopping...");
+            }
+        }
+    }
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseWelcomePage();
+        }
+    }
+}
