d9e51b883690189ef82e7b6930ab9b83c17688b2
shizzlator <davidmiranda11@gmail.com>
2014-04-07 12:18:04 +0100

    added missing files and ignored dll.config

--- Vapour.Domain/TestOutput.cs
+++ Vapour.Domain/TestOutput.cs
@@ -1,9 +1,14 @@
-﻿using NUnit.Core;
+﻿using System.Collections;
+using System.Runtime.Serialization;
+using NUnit.Core;
 
 namespace Vapour.Domain
 {
+    [KnownType(typeof(TestResult))]
+    [KnownType(typeof(ArrayList))]
     public class TestOutput
     {
+        [DataMember]
         public TestResult TestResult { get; set; }
     }
 }
