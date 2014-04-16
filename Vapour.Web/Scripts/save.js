f3dfd1c63183ab1133e16597166148f90a7afae3
David Miranda <davidmiranda11@gmail.com>
2014-04-16 01:04:27 +0100

    did some styling

--- Vapour.Web/Scripts/save.js
+++ Vapour.Web/Scripts/save.js
@@ -1,4 +1,5 @@
 ﻿$(function () {
+    //SAVE PROJECT CONFIG
     $('#save').click(function (){
         $.ajax({
             url: '',
@@ -10,12 +11,49 @@
             }
         });
     });
+
+    //UPLOAD ASSEMBLY
+    $('#upload').click(function () {
+        var formData = new FormData($('form')[0]);
+        var request = $.ajax({
+            url: 'new/project/FakeProject/Development/Smoke',  //Server script to process data
+            type: 'POST',
+            xhr: function () {  // Custom XMLHttpRequest
+                var myXhr = $.ajaxSettings.xhr();
+                if (myXhr.upload) { // Check if upload property exists
+                    myXhr.upload.addEventListener('progress', progressHandlingFunction, false); // For handling the progress of the upload
+                }
+                return myXhr;
+            },
+            // Form data
+            error: function () {
+                alert(request.responseText);
+            },
+            data: formData,
+            //Options to tell jQuery not to process data or worry about content-type.
+            cache: false,
+            contentType: false,
+            processData: false,
+            success: function () { window.Location = "/Projects/home"; }
+        });
+    });
 });
 
 function moveToAssemblyUploadScreen() {
     $('#save').hide();
     $('#projectConfiguration').fadeOut(function() {
-        $('#assemblyUpload').fadeIn();
+        $('.upload-container').fadeIn();
         $('progress').fadeIn();
     });
 }
+
+function progressHandlingFunction(e) {
+    if (e.lengthComputable) {
+        $('progress').attr({ value: e.loaded, max: e.total });
+    }
+}
+
+//TODO: jQuery
+document.getElementById("chooseFile").onchange = function () {
+    document.getElementById("uploadFile").value = this.value;
+};
