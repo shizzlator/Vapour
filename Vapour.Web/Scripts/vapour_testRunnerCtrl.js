fc60c23fda3cd93a4b4e9aa2c0216000fb9bc405
Piotrek Dąbrowski <floubadour@gmail.com>
2014-05-28 10:53:16 +0200

    improved TestRunner page

---
+++ Vapour.Web/Scripts/vapour_testRunnerCtrl.js
@@ -0,0 +1,29 @@
+﻿/*global console:false, angular:false */
+var app = angular.module('vapour', [])
+  .config(function ( $httpProvider) {
+      delete $httpProvider.defaults.headers.common['X-Requested-With'];
+  });
+
+
+app.controller('testRunnerCtrl', function($scope, $http, $filter) {
+  var self = this;
+
+  $scope.loading = true;
+
+  $scope.initialize = function(apiUrl) {
+
+    $http.jsonp(apiUrl + '?callback=JSON_CALLBACK').
+      success(function(data, status, headers, config) {
+        $scope.loading = false;
+
+        $scope.results = {
+          success: data.Success,
+          content: data.Message,
+          failedTests: data.FailedTests
+        };
+      }).
+      error(function(data, status, headers, config) {
+        console.log('Error: ' + data + ', Status: ' + status);
+      });
+  };
+});
